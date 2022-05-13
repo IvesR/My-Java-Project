00cfdf9dba4bd8a3af1a078865e727bb3677c953
Oy Ives <oy@localhost>
2022-05-13 10:58:57 -0700

    First Commit

---
+++ as1/src/as1/Message.java
@@ -0,0 +1,16 @@
+package as1;
+
+import javax.swing.JOptionPane;
+
+public class Message {
+
+	public static void main(String[] args) {
+		String msg;
+		msg="Today is a good day.\n";
+		msg=msg + "I am learining Java programming.\n";
+		msg=msg + "It's a lot of fun.\n";
+		JOptionPane.showMessageDialog(null, msg);
+
+	}
+
+}
